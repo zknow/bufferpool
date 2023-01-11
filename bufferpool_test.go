7e28535786140ab831826860f80eec430d8099ce
ArZhi Chen <zhi711825@gmail.com>
2023-01-11 16:40:05 +0800

    remove main.go and add bufferpool_test.go

--- bufferpool_test.go
+++ bufferpool_test.go
@@ -1,26 +1,26 @@
-package main
+package bufferpool
 
 import (
 	"bytes"
-	"log"
+	"testing"
 
-	"github.com/zknow/bufferpool/bufferpool"
+	"github.com/stretchr/testify/assert"
 )
 
-func main() {
+func TestRouterGroupBasic(t *testing.T) {
 	poolSize := 3
 
-	bfPool := bufferpool.NewBufferPool(poolSize)
+	bfPool := NewBufferPool(poolSize)
 
 	// 空的pool pool len=0
 	cnt := bfPool.GetFreeBufferCount()
-	log.Println(cnt) // count: 0
+	assert.Equal(t, cnt, 0)
 
 	// 產生一個buffer並回收後 pool len=1
 	bf1 := bytes.NewBuffer(make([]byte, 0))
 	bfPool.Put(bf1)
 	cnt = bfPool.GetFreeBufferCount()
-	log.Println(cnt) // count: 1
+	assert.Equal(t, cnt, 1)
 
 	// 產生兩個buffer並回收後 pool len=3
 	bf2 := bytes.NewBuffer(make([]byte, 0))
@@ -28,11 +28,11 @@ func main() {
 	bfPool.Put(bf2)
 	bfPool.Put(bf3)
 	cnt = bfPool.GetFreeBufferCount()
-	log.Println(cnt) // count: 3
+	assert.Equal(t, cnt, 3)
 
 	// 達到pool size上限新buffer放入會失敗
 	bf4 := bytes.NewBuffer(make([]byte, 0))
 	bfPool.Put(bf4)
 	cnt = bfPool.GetFreeBufferCount()
-	log.Println(cnt) // count: 3
+	assert.Equal(t, cnt, 3)
 }
